4dc12d76b2ddb26f993b7d84c2692499dd639a25
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2020-05-05 16:02:57 +0900

    kmeans isolatinforest silhouette

--- anomaly_detection/IsolationForest.py
+++ anomaly_detection/IsolationForest.py
@@ -1,5 +1,101 @@
+"""ライブラリ"""
+import pandas as pd
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
+import seaborn as sns
+from statistics import mean, variance
+from scipy import stats
+from scipy.stats import norm
+from collections import Counter
+import copy
+import pickle
 import treelib
+
+
+"""データ生成関数"""
+def generate_data(directory, data_n, aged_data_n):
+    data = []
+    aged_data = []
+    
+    for i in range(1, data_n+1):
+        tmp_data = pd.read_csv(directory+'/s'+str(i)+'.csv', header=None).values
+        data.append(tmp_data)
+    
+    for i in range(1, aged_data_n+1):
+        tmp_data = pd.read_csv(directory+'/s'+str(i)+'_aged.csv', header=None).values
+        aged_data.append(tmp_data)
+    
+    data = np.array(data)
+    aged_data = np.array(aged_data)
+    
+    return data, aged_data
+
+newdata, ageddata = generate_data('fresh_aged_ieice', 50, 2)
+
+
+"""0の数数える関数 二次元入れろ"""
+def count_zero(data):
+    
+    tmp = []
+    for i in range(data.shape[0]):
+        a = (data[i].shape[0] * data[i].shape[1]) - np.count_nonzero(data[i])
+        tmp.append(a)
+        
+    return tmp
+
+"""９２０この値を消す"""
+def delete_920(data, check):
+    new = np.zeros_like(data)
+    counter = 0
+    for i in data:
+        for j in range(i.shape[0]):
+            for k in range(i.shape[1]):
+                if [j, k] in check:
+                    new[counter, j, k] = 0 
+                else:
+                    new[counter, j, k] = data[counter, j, k]
+                    
+        counter += 1
+                    
+    return new
+
+
+"""一次元にする関数"""
+def change_flatten(data):
+    tmp = []
+    for i in range(data.shape[0]):
+        tmp.append(data[i].flatten())
+        
+    tmp = np.array(tmp)
+    
+    return tmp
+
+
+"""0を消す関数 flatteしたやつ入れろ"""
+def delete_zero(data):
+    tmp = []
+    for i in range(data.shape[0]):
+        tmp2 = copy.deepcopy(data[i])
+        tmp.append(tmp2[tmp2 != 0])
+        
+    tmp = np.array(tmp)
+    
+    return tmp
+
+
+"""リストを繋げる関数"""
+def connect(a, b):
+    tmp = []
+    for i in range(a.shape[0]):
+        tmp.append(a[i])
+        
+    for i in range(b.shape[0]):
+        tmp.append(b[i])
+    
+    tmp = np.array(tmp)
+    return tmp
 
 class IsolateTree:
     class __data:
@@ -109,3 +205,41 @@ class IsolateForest:
             abnormal_scores[:, i] = self.__trees[i].get_abnormal_score(X)
 
         return np.mean(abnormal_scores, axis=1)
+
+
+def main():
+    fresh, aged = generate_data('fresh_aged_ieice', 50, 2) # (50, 148, 33) (2, 148, 33)
+
+    newdata = connect(fresh, aged) # (52, 148, 33)
+    check_FPGA = []
+    for i in range(148):
+        for j in range(33):
+            if fresh[0, i, j] == 0:
+                check_FPGA.append([i,j])
+    freshaged = delete_920(newdata, check_FPGA) #(52, 148, 33)
+
+    new_freshaged = change_flatten(freshaged) # (52 4884)
+
+    nonzero_freshaged = delete_zero(new_freshaged) #(52, 3964)
+
+    a = IsolateForest()
+    a.fit(nonzero_freshaged, 1000, 26)
+
+    ans = a.predict(nonzero_freshaged)
+
+    print(ans)
+    isaka = []
+    for i in range(len(ans)):
+        isaka.append([ans[i], i])
+
+    isaka.sort()
+    print(isaka)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+    pass
+
