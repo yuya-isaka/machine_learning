03baeef9eb4d4b94727595bc2e69eaaac5b31fb9
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2020-06-13 19:45:31 +0900

    gitignore fresh_aged_ieice

--- other_file/test.py
+++ other_file/test.py
@@ -1,6 +1,7 @@
 import sys, os
 sys.path.append(os.pardir)
 import pandas as pd
+import numpy as np
 
 test = pd.read_csv('fresh_aged_ieice/s1.csv', header=None)
 
@@ -10,3 +11,5 @@ print(type(test))
 test_value= test.values
 print(test_value)
 print(test_value.shape)
+a = test_value[np.nonzero(test_value)]
+print(len(a))
